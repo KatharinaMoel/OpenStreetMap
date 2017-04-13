2cd347436a1ea3b60b9cb03e81eda9105af953a6
Katharina Moel <k.moelter@fu-berlin.de>
2017-04-13 03:25:34 +0200

    PostalAreas: add data with rectangles that contain postal code areas (edges are given by min/max lats resp. lons of areas).

--- PostalAreas.py
+++ PostalAreas.py
@@ -11,6 +11,22 @@ class PostalAreas(object):
         self.area_lats, self.area_lons = area_lats, area_lons
         self.cameras = pd.DataFrame(cameras, index = ['lat', 'lon']).T
         print(self.cameras.head())
+        self.cube_lats = pd.DataFrame( {postal: [ min(self.area_lats[postal]) , max(self.area_lats[postal]) ] for postal in areas.keys()},
+                                       index = ['min', 'max'] ).T
+        self.cube_lons = pd.DataFrame( {postal: [ min(self.area_lons[postal]) , max(self.area_lons[postal]) ] for postal in areas.keys()},
+                                       index = ['min', 'max'] ).T
+
+    def get_cubes_to_cam(self, cam_id):
+        # get lat and lon of camera
+        cam_lat = self.cameras[cam_id]['lat']
+        cam_lon = self.cameras[cam_id]['lon']
+        # get rows of cube dataframes that 'contain' the camera in one direction (lat resp. lon)
+        cubes_to_cam_lats = self.cube_lats.loc[ (self.cube_lats['min'] <= cam_lat) & (self.cube_lats['max'] >= cam_lat) ]
+        cubes_to_cam_lons = self.cube_lons.loc[ (self.cube_lons['min'] <= cam_lon) & (self.cube_lons['max'] >= cam_lon) ]
+        # get only rows of cubes that contain the camera in both lat and lon direction
+        cubes_to_cam = cubes_to_cam_lats.loc[ cubes_to_cam_lats.intersection(cubes_to_cam_lons.index) ]
+        # return the containing cubes (by index of postal codes)
+        return cubes_to_cam.index
 
 #############################################################################################################################
 
